abdf28798ee3f4cf9115cd7d631b6f8fb56b7fe4
刘文超 <liuwenchao1990@163.com>
2017-02-09 18:18:19 +0800

    初始化代码

---
+++ pages/index/index.js
@@ -0,0 +1,23 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+  
+  },
+  onLoad: function () {
+    console.log("---index onload---")
+  },
+  onShow:function(){
+    console.log("---index onShow----")
+  },
+  onReady:function(){
+    console.log("---index onReady---")
+  },
+  onHide:function(){
+    console.log("---index onHide----")
+  },
+  onUnload:function(){
+    console.log("----index onUnload----")
+  }
+})
