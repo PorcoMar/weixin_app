7d32b7cb86b0d0c10a772990aa0cf38c0ae4b819
goldswallow <lzl102872@163.com>
2017-02-14 13:35:48 +0800

    提交订单页面

--- pages/order/pages/submit-order/submit-order.js
+++ pages/order/pages/submit-order/submit-order.js
@@ -1,6 +1,12 @@
 // pages/order/pages/submit-order/submit-order.js
 Page({
-  data:{},
+  data:{
+    member:{
+      name:"韩梅梅",
+      phone:14253698532,
+      level:"高级"
+    }
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
