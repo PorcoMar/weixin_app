c57516067264b9b1d03bff33b77b426c816a3ace
刘文超 <liuwenchao1990@163.com>
2017-02-15 17:56:20 +0800

    删除垃圾文件

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -2,73 +2,63 @@
 var app = getApp();
 var HOST = app.globalData.HOST;
 Page({
-  data:{
-    userInfo:null
+  data: {
+    userInfo: null
   },
-  onLoad:function(options){
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
-    this.setData({userInfo:app.globalData.userInfo});
+    this.setData({ userInfo: app.globalData.userInfo });
     console.log(this.data);
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
   },
   //点击个人余额
-  userBalance:function(){
+  userBalance: function () {
     wx.navigateTo({
-      url:"./balance/balance"
+      url: "./balance/balance"
     })
   },
   //点击个人信息
-  userInfo:function(){
+  userInfo: function () {
     wx.navigateTo({
-      url:"./userInfo/userInfo"
+      url: "./userInfo/userInfo"
     })
   },
   //登录
-  login:function(){
+  login: function () {
     var that = this;
-    wx.showModal({
-      title:"微信授权",
-      content:"",
-      cancelText:"拒绝",
-      success:function(res){
-        if(res.confirm){
-            //获取微信账号信息
-            wx.login({
-              success:function(){
-                wx.getUserInfo({
-                  success:function(res){
-                    console.log(res);
-                    var userInfo = JSON.parse(res.rawData);
-                    app.globalData.userInfo = userInfo;
-                    console.log(userInfo);
-                    that.setData({userInfo:userInfo})
-                  }
-                })
-              }
-            })
-        }
+    wx.login({
+      success: function () {
+        wx.getUserInfo({
+          success: function (res) {
+            console.log(res);
+            var userInfo = JSON.parse(res.rawData);
+            app.globalData.userInfo = userInfo;
+            console.log(userInfo);
+            that.setData({ userInfo: userInfo })
+          }
+        })
       }
     })
   },
   //绑定手机号
-  bindPhone:function(){
+  bindPhone: function () {
     wx.navigateTo({
       url: './bindPhone/bindPhone'
     })
   },
   //点击二维码
-  twoBarCode:function(){
+  twoBarCode: function () {
     wx.navigateTo({
       url: './twoBarCode/twoBarCode'
     })
