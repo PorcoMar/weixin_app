a8cd5f596e5d23d1414e943ca965919fc746a390
刘文超 <liuwenchao1990@163.com>
2017-02-16 16:12:23 +0800

    提交用户登录代码

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,34 @@
 //index.js
 //获取应用实例
-var app = getApp()
+var app = getApp();
+var HOST = app.globalData.HOST;
 Page({
   data: {
     shopName:"Cocodemer港汇店",
     shopImage:"../../images/homePage/shop_image.jpg"
   },
   onLoad: function () {
-    console.log("---index onload---")
+    console.log("---index onload---");
+    var location = app.globalData.location;
+    console.log(location);
+    wx.request({
+        url:HOST + "/shop/detail",
+        data:{
+          lng:location.longitude,
+          lat:location.latitude
+        },
+        header:{
+        "Content-Type":"application/x-www-form-urlencoded"
+        },
+        method:"POST",
+        success:function(res){
+          console.log(res);
+          console.log("------get shop successed-----");
+        },
+        fail:function(){
+          console.log("------get shop fail-----")
+        }
+    })
   },
   onShow:function(){
     console.log("---index onShow----")
