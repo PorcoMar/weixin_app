496ce0439fd100dc51ec8ac8068bae7d6d44ed76
Porco_Mar <990135141@qq.com>
2017-07-06 11:40:33 +0800

    会员中心首页

--- pages/VIPCenter/VIPCenter.js
+++ pages/VIPCenter/VIPCenter.js
@@ -3,7 +3,8 @@ var app = getApp();
 var HOST = app.globalData.HOST;
 Page({
   data:{
-
+    wxInfo: null,
+    userInfo: null
   },
   onLoad:function(options){
     //获取用户信息
@@ -23,7 +24,38 @@ Page({
     // 页面渲染完成
   },
   onShow:function(){
+    console.log(app.globalData.wxInfo)
     var that = this;
+    if (app.globalData.wxInfo) {
+      this.setData({ wxInfo: app.globalData.wxInfo });
+      wx.getStorage({
+        key: "HEADER",
+        success: function (res) {
+          console.log("------get header success-------");
+          //获取用户信息
+
+          getApp().globalData.HEADER = res.data;
+
+          wx.request({
+            url: HOST + "/user/info",
+            method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+            header: res.data, // 设置请求的 header
+            success: function (res) {
+              if (res.data.code == "0") {
+                console.log(res.data.result);
+                that.setData({ userInfo: res.data.result });
+              }
+            }
+          })
+        }
+      })
+    } else {
+      wx.showModal({
+        title: '提示',
+        content: '请删除小程序后重新进入，使用微信授权登录',
+        showCancel: false
+      });
+    };
     wx.getStorage({
       key:"HEADER",
       success:function(res){
@@ -34,7 +66,7 @@ Page({
           method:"POST",
           header:res.data,
           success:function(res){
-
+            console.log(res)
           }
         });
       }
